d4615304295ce04ea5b33dc311275020eb691772
Aniket Kumar <aniket95767@gmail.com>
2025-06-16 11:15:07 +0530

    Update defaul_param.py

--- Functions/defaul_param.py
+++ Functions/defaul_param.py
@@ -4,4 +4,4 @@ cal_multiply(2,3)
 cal_multiply() #in this acse the default value of a and b are passsed
 def cal_div(a,b=1):#this is also valid
     print(5)
-#def cal_div(a=1,b): this is invalid as default parameters should not be the first parameter
+#def cal_div(a=1,b): this is invalid as non default parameters should not be the last parameter
